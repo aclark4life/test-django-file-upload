1638f1844c755bc242185511a15914628d8ca3e8
Alex Clark <aclark@aclark.net>
2023-01-31 16:21:29 -0500

    Add frontend

---
+++ project/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-1u9g2)+f2c!0h5wg!g4&d=g5+h-#@5$-tmwg4jx3$7ri(+ukiu"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
